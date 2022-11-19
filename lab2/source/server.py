6a2b51d1b130f5ae8e0d08183ee559c703511bef
byczus1990 <byczek.szymon@gmail.com>
2022-11-19 12:22:07 +0100

    added query params handiling for all features

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -16,19 +16,26 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 
         print(self.path)
         params = parse_qs(urlparse(self.path).query)
-        print(params) 
+        #print(params) 
         
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
-            now = datetime.now()
-            t = time.localtime()
-            new_time = now + timedelta(hours=1)
-            current_time = new_time.strftime("%H:%M:%S")   
-            self.wfile.write(str.encode(current_time))
+            self.end_headers()
+            
+            if not params:            
+                self.wfile.write(b"Hello World!\n")
+            elif(params['cmd'] == ['time']):
+                now = datetime.now()
+                t = time.localtime()
+                new_time = now + timedelta(hours=1)
+                current_time = new_time.strftime("%H:%M:%S")   
+                self.wfile.write(str.encode(current_time))                       
+            elif(params.get('cmd', None) == ['rev']):
+                reverseString = query_params.get('str', None)[0]
+                if reverseString:
+                    self.wfile.write(str.encode(reverseString[::-1]))
         else:
             super().do_GET()
     
