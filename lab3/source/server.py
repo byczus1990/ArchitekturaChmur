466d4062a2075a5eb2714d895bea0afbd8d8b3ca
byczus1990 <byczek.szymon@gmail.com>
2022-11-26 11:53:25 +0100

    added special signs counting

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -20,6 +20,7 @@ class web_server(http.server.SimpleHTTPRequestHandler):
         print(string_from_query) 
         
         if self.path == '/':
+#            print("IM in path")
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
@@ -27,6 +28,7 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             
             if string_from_query:
                 local_string = string_from_query[0]
+ #               print("IM in loop")
                 answer = {"lowercase":0, "uppercase":0, "digits":0, "special":0}
 
                 for i in range(len(local_string)):
@@ -36,8 +38,10 @@ class web_server(http.server.SimpleHTTPRequestHandler):
                         answer["lowercase"] += 1
                     elif local_string[i].isdigit():
                         answer["digits"] += 1
+                    else:
+                        answer["special"] += 1
 
-            self.wfile.write(str.encode(json.dumps(answer)))
+                self.wfile.write(str.encode(json.dumps(answer)))
 
         else:
             super().do_GET()
