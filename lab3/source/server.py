2b429aa81d7c3a69e325e97d01fc60633a928fd7
byczus1990 <byczek.szymon@gmail.com>
2022-11-26 12:09:11 +0100

    fixed some indents, cleaned some code

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -4,32 +4,25 @@ import socketserver
 import json
 from urllib.parse import urlparse, parse_qs
 
-#print('source code for "http.server":', http.server.__file__)
-
 class web_server(http.server.SimpleHTTPRequestHandler):
     
-    
-    
     def do_GET(self):
-
-#        print(self.path)
-        path = urlparse(self.path) 
-        params = parse_qs(path.query)
+ 
+        path = urlparse(self.path)
+        params = parse_qs(path.query) 
         string_from_query = params.get('str', None)
         print("STRING PASSED AS QUERY:")
         print(string_from_query) 
         
-        if self.path == '/':
-#            print("IM in path")
+        if path.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()
-            
+
             if string_from_query:
                 local_string = string_from_query[0]
- #               print("IM in loop")
-                answer = {"lowercase":0, "uppercase":0, "digits":0, "special":0}
+                answer =  { "lowercase" : 0, "uppercase" : 0, "digits" : 0, "special" : 0}
 
                 for i in range(len(local_string)):
                     if local_string[i].isupper():
@@ -40,7 +33,7 @@ class web_server(http.server.SimpleHTTPRequestHandler):
                         answer["digits"] += 1
                     else:
                         answer["special"] += 1
-
+            
                 self.wfile.write(str.encode(json.dumps(answer)))
 
         else:
